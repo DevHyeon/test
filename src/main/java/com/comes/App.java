efd4790ec602e0265afa21c64a5af0ae55a08173
DESKTOP-7K61OQD\USER <yshss1@naver.com>
2021-04-22 17:37:03 +0900

    java maven 강의

---
+++ src/main/java/com/comes/App.java
@@ -0,0 +1,17 @@
+package com.comes;
+
+import com.comes.examilb.entity.Exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Eclipse Maven!" );
+        Exam exam = new Exam(10,20,30);
+        System.out.println(exam.total());
+    }
+}
